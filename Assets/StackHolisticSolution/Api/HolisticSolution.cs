01e7b05cebec0b002e21c27b40ad5751d355d229
KulArtsiom <kulartsiom@gmail.com>
2021-02-24 17:01:44 +0300

    Update HolisticSolution.cs

--- Assets/StackHolisticSolution/Api/HolisticSolution.cs
+++ Assets/StackHolisticSolution/Api/HolisticSolution.cs
@@ -10,7 +10,7 @@ namespace StackHolisticSolution.Api
     [SuppressMessage("ReSharper", "InconsistentNaming")]
     public class HSAppodealConnector
     {
-        public const string HolisticSolutionPluginVersion = "1.0.5";
+        public const string HolisticSolutionPluginVersion = "1.0.6";
 
         private readonly IHSAppodealConnector nativeHSAppodealConnector;
 
