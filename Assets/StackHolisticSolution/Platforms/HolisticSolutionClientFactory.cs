db9a41014a5054b00c085f5353210138eeca51b4
KulArtsiom <kulartsiom@gmail.com>
2021-06-28 11:41:44 +0300

    Update HolisticSolutionClientFactory.cs

--- Assets/StackHolisticSolution/Platforms/HolisticSolutionClientFactory.cs
+++ Assets/StackHolisticSolution/Platforms/HolisticSolutionClientFactory.cs
@@ -2,8 +2,6 @@
 
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using StackHolisticSolution.Common;
-using StackHolisticSolution.Platforms.Android;
 
 
 namespace StackHolisticSolution.Platforms
@@ -11,73 +9,7 @@ namespace StackHolisticSolution.Platforms
     [SuppressMessage("ReSharper", "InconsistentNaming")]
     public class HolisticSolutionClientFactory
     {
-        internal static IHSAppodealConnector GetHSAppodealConnector()
-        {
-#if UNITY_ANDROID && !UNITY_EDITOR
-			return new AndroidHSAppodealConnector();
-#elif UNITY_IPHONE && !UNITY_EDITOR
-            return new iOS.iOSHSAppodealConnector();
-#else
-            return new Dummy.Dummy();
-#endif
-        }
-        
-        internal static IHSAppsflyerService GetHSAppsflyerService(string key)
-        {
-#if UNITY_ANDROID && !UNITY_EDITOR
-			return new AndroidHSAppsflyerService(key);
-#elif UNITY_IPHONE && !UNITY_EDITOR
-            return null;
-#else
-            return new Dummy.Dummy();
-#endif
-        }
-        
-        internal static IHSAppsflyerService GetHSAppsflyerService(string devKey, string appId, string keys)
-        {
-#if UNITY_ANDROID && !UNITY_EDITOR
-			return null;
-#elif UNITY_IPHONE && !UNITY_EDITOR
-            return new iOS.iOSHSAppsflyerService(devKey, appId, keys);
-#else
-            return new Dummy.Dummy();
-#endif
-        }
-        
-        internal static IHSFirebaseService GetHSFirebaseService(string defaults, long expirationDuration)
-        {
-#if UNITY_ANDROID && !UNITY_EDITOR
-			return null;
-#elif UNITY_IPHONE && !UNITY_EDITOR
-            return new iOS.iOSHSFirebaseService(defaults, expirationDuration);
-#else
-            return new Dummy.Dummy();
-#endif
-        }
-        
-        internal static IHSFirebaseService GetHSFirebaseService()
-        {
-#if UNITY_ANDROID && !UNITY_EDITOR
-			return new AndroidHSFirebaseService();
-#elif UNITY_IPHONE && !UNITY_EDITOR
-            return null;
-#else
-            return new Dummy.Dummy();
-#endif
-        }
-        
-        
-        internal static IHSFacebookService GetHSFacebookService()
-        {
-#if UNITY_ANDROID && !UNITY_EDITOR
-			return new AndroidHSFacebookService();
-#elif UNITY_IPHONE && !UNITY_EDITOR
-            return new iOS.iOSHSFacebookService();
-#else
-            return new Dummy.Dummy();
-#endif
-        }
-        
+
         internal static IHSAppConfig GetHSAppConfig()
         {
 #if UNITY_ANDROID && !UNITY_EDITOR
