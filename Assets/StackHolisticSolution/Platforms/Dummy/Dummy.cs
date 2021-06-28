82b8beea3c04bf37fc9f9fafd685fa7d382d1a1b
KulArtsiom <kulartsiom@gmail.com>
2021-06-28 11:41:45 +0300

    Update Dummy.cs

--- Assets/StackHolisticSolution/Platforms/Dummy/Dummy.cs
+++ Assets/StackHolisticSolution/Platforms/Dummy/Dummy.cs
@@ -1,148 +1,111 @@
 // ReSharper disable All
 using System;
 using System.Collections.Generic;
-using StackHolisticSolution.Api;
-using StackHolisticSolution.Common;
-using StackHolisticSolution.Platforms.Android;
 using UnityEngine;
 
 namespace StackHolisticSolution.Platforms.Dummy
 {
-    public class Dummy : IHSAppodealConnector, IHSAppsflyerService, IHSFirebaseService,
-        IHSAppConfig, IHSApp, IHSError, IHSFacebookService, IHSInAppPurchaseBuilder, IHSLogger
+    public class Dummy : 
+        IHSAppConfig, IHSApp, IHSError, IHSInAppPurchaseBuilder, IHSLogger
     {
         private const string DummyMessage = "Not supported on this platform";
-        
-        void IHSAppodealConnector.setEventsEnabled(bool value)
-        {
-            Debug.Log(DummyMessage);
-        }
 
-        public AndroidJavaObject GetAndroidInstance()
-        {
-            Debug.Log(DummyMessage);
-            return null;
-        }
 
-        public IntPtr GetIntPtr()
+        public void setComponentInitializeTimeout(long value)
         {
-            Debug.Log(DummyMessage);
-            return new IntPtr();
-        }
-
-        void IHSFacebookService.setEventsEnabled(bool value)
-        {
-            Debug.Log(DummyMessage);
-        }
-
-        void IHSFirebaseService.setEventsEnabled(bool value)
-        {
-            Debug.Log(DummyMessage);
-        }
-
-        void IHSAppsflyerService.setEventsEnabled(bool value)
-        {
-            Debug.Log(DummyMessage);
-        }
-
-        public void withConnectors(HSAppodealConnector hsAppodealConnector)
-        {
-            Debug.Log(DummyMessage);
-        }
-
-        public void withServices(params IHSService[] services)
-        {
-            Debug.Log(DummyMessage);
+            throw new NotImplementedException();
         }
 
         public void setDebugEnabled(bool value)
         {
-            Debug.Log(DummyMessage);
+            throw new NotImplementedException();
         }
 
-        public void setComponentInitializeTimeout(long value)
+        public void setLoggingEnabled(bool value)
         {
-            Debug.Log(DummyMessage);
+            throw new NotImplementedException();
+        }
+
+        public void setAppKey(string appKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setAdType(int adType)
+        {
+            throw new NotImplementedException();
         }
 
         public void initialize(HSAppConfig appConfig, IHSAppInitializeListener hsAppInitializeListener)
         {
-            Debug.Log(DummyMessage);
+            throw new NotImplementedException();
         }
 
         public void logEvent(string key, Dictionary<string, object> dictionary)
         {
-            Debug.Log(DummyMessage);
+            throw new NotImplementedException();
         }
 
         public void logEvent(string key)
         {
-            Debug.Log(DummyMessage);
+            throw new NotImplementedException();
         }
 
         public void validateInAppPurchaseAndroid(HSInAppPurchase purchase,
             IHSInAppPurchaseValidateListener hsInAppPurchaseValidateListener)
         {
-            Debug.Log(DummyMessage);
+            throw new NotImplementedException();
         }
 
         public void validateInAppPurchaseiOS(string productIdentifier, string price, string currency, string transactionId,
             string additionalParams, IInAppPurchaseValidationiOSCallback inAppPurchaseValidationiOSCallback)
         {
-            Debug.Log(DummyMessage);
-        }
-
-        public void validateInAppPurchaseiOS(string productIdentifier, string price, string currency, string transactionId,
-            string additionalParams)
-        {
-            Debug.Log(DummyMessage);
+            throw new NotImplementedException();
         }
 
         public string toString()
         {
-            Debug.Log(DummyMessage);
-            return string.Empty;
+            throw new NotImplementedException();
         }
 
         public IHSInAppPurchase build()
         {
-            Debug.Log(DummyMessage);
-            return null;
+            throw new NotImplementedException();
         }
 
         public void withAdditionalParams(Dictionary<string, string> additionalParameters)
         {
-            Debug.Log(DummyMessage);
+            throw new NotImplementedException();
         }
 
         public void withCurrency(string currency)
         {
-            Debug.Log(DummyMessage);
+            throw new NotImplementedException();
         }
 
         public void withPrice(string price)
         {
-            Debug.Log(DummyMessage);
+            throw new NotImplementedException();
         }
 
         public void withPurchaseData(string purchaseData)
         {
-            Debug.Log(DummyMessage);
+            throw new NotImplementedException();
         }
 
         public void withSignature(string signature)
         {
-            Debug.Log(DummyMessage);
+            throw new NotImplementedException();
         }
 
         public void withPublicKey(string publicKey)
         {
-            Debug.Log(DummyMessage);
+            throw new NotImplementedException();
         }
 
         public void setEnabled(bool value)
         {
-            Debug.Log(DummyMessage);
+            throw new NotImplementedException();
         }
     }
 }
