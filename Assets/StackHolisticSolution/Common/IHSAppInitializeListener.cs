ba566617af95880f9bdd574b25e00485743706ca
KulArtsiom <kulartsiom@gmail.com>
2021-07-19 11:20:29 +0300

    Update IHSAppInitializeListener.cs

--- Assets/StackHolisticSolution/Common/IHSAppInitializeListener.cs
+++ Assets/StackHolisticSolution/Common/IHSAppInitializeListener.cs
@@ -6,8 +6,6 @@ namespace StackHolisticSolution
     [SuppressMessage("ReSharper", "InconsistentNaming")]
     public interface IHSAppInitializeListener
     {
-        void onAppInitializeFailed(IEnumerable<HSError> hsErrors);
-        void onAppInitializeFailed(string error);
-        void onAppInitialized();
+        void onAppInitialized(string error);
     }
 }
