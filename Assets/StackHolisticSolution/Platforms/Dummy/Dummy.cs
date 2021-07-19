9e844b535820642e055e5a00199e1655a65e2a98
KulArtsiom <kulartsiom@gmail.com>
2021-07-19 12:51:59 +0300

    Update Dummy.cs

--- Assets/StackHolisticSolution/Platforms/Dummy/Dummy.cs
+++ Assets/StackHolisticSolution/Platforms/Dummy/Dummy.cs
@@ -1,138 +1,142 @@
 // ReSharper disable All
+
 using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace StackHolisticSolution.Platforms.Dummy
 {
-    public class Dummy : 
+    public class Dummy :
         IHSAppConfig, IHSApp, IHSError, IHSInAppPurchaseBuilder
     {
-        private const string DummyMessage = "Not supported on this platform";
-
-
+        private const string DummyMessage = "To test advertising, install your application on the Android/iOS device.";
+        
         public void setComponentInitializeTimeout(long value)
         {
-            throw new NotImplementedException();
+            Debug.Log("Call to setComponentInitializeTimeout on not supported platform." + DummyMessage);
         }
 
         public void setDebugEnabled(bool value)
         {
-            throw new NotImplementedException();
+            Debug.Log("Call to setDebugEnabled on not supported platform." + DummyMessage);
         }
 
         public void setLoggingEnabled(bool value)
         {
-            throw new NotImplementedException();
+            Debug.Log("Call to setLoggingEnabled on not supported platform." + DummyMessage);
         }
 
         public void setAppKey(string appKey)
         {
-            throw new NotImplementedException();
+            Debug.Log("Call to setAppKey on not supported platform." + DummyMessage);
         }
 
         public void setAdType(int adType)
         {
-            throw new NotImplementedException();
+            Debug.Log("Call to setAdType on not supported platform." + DummyMessage);
         }
 
         public void initialize(HSAppConfig appConfig, IHSAppInitializeListener hsAppInitializeListener)
         {
-            throw new NotImplementedException();
+            Debug.Log("Call to initialize on not supported platform." + DummyMessage);
         }
 
         public void logEvent(string key, Dictionary<string, object> dictionary)
         {
-            throw new NotImplementedException();
+            Debug.Log("Call to logEvent on not supported platform." + DummyMessage);
         }
 
         public void logEvent(string key)
         {
-            throw new NotImplementedException();
+            Debug.Log("Call to logEvent on not supported platform." + DummyMessage);
         }
 
         public string getVersion()
         {
-            throw new NotImplementedException();
+            Debug.Log("Call to getVersion on not supported platform." + DummyMessage);
+            return string.Empty;
         }
 
         public bool isInitialized()
         {
-            throw new NotImplementedException();
+            Debug.Log("Call to isInitialized on not supported platform." + DummyMessage);
+            return false;
         }
 
         public void validateInAppPurchaseAndroid(HSInAppPurchase purchase,
             IHSInAppPurchaseValidateListener hsInAppPurchaseValidateListener)
         {
-            throw new NotImplementedException();
+            Debug.Log("Call to validateInAppPurchaseAndroid on not supported platform." + DummyMessage);
         }
 
-        public void validateInAppPurchaseiOS(string productIdentifier, string price, string currency, string transactionId,
+        public void validateInAppPurchaseiOS(string productIdentifier, string price, string currency,
+            string transactionId,
             string additionalParams, iOSPurchaseType type,
             IInAppPurchaseValidationiOSCallback inAppPurchaseValidationiOSCallback)
         {
-            throw new NotImplementedException();
+            Debug.Log("Call to validateInAppPurchaseiOS on not supported platform." + DummyMessage);
         }
-
-
+        
         public string toString()
         {
-            throw new NotImplementedException();
+            Debug.Log("Call to toString on not supported platform." + DummyMessage);
+            return string.Empty;
         }
 
         public void withPublicKey(string publicKey)
         {
-            throw new NotImplementedException();
+            Debug.Log("Call to withPublicKey on not supported platform." + DummyMessage);
         }
 
         public void withSignature(string signature)
         {
-            throw new NotImplementedException();
+            Debug.Log("Call to withSignature on not supported platform." + DummyMessage);
         }
 
         public void withPurchaseData(string purchaseData)
         {
-            throw new NotImplementedException();
+            Debug.Log("Call to withPurchaseData on not supported platform." + DummyMessage);
         }
 
         public void withPrice(string price)
         {
-            throw new NotImplementedException();
+            Debug.Log("Call to withPrice on not supported platform." + DummyMessage);
         }
 
         public void withCurrency(string currency)
         {
-            throw new NotImplementedException();
+            Debug.Log("Call to withCurrency on not supported platform." + DummyMessage);
         }
 
         public void withSku(string sku)
         {
-            throw new NotImplementedException();
+            Debug.Log("Call to withSku on not supported platform." + DummyMessage);
         }
 
         public void withOrderId(string orderId)
         {
-            throw new NotImplementedException();
+            Debug.Log("Call to withOrderId on not supported platform." + DummyMessage);
         }
 
         public void withPurchaseToken(string purchaseToken)
         {
-            throw new NotImplementedException();
+            Debug.Log("Call to withPurchaseToken on not supported platform." + DummyMessage);
         }
 
         public void withPurchaseTimestamp(long purchaseTimestamp)
         {
-            throw new NotImplementedException();
+            Debug.Log("Call to withPurchaseTimestamp on not supported platform." + DummyMessage);
         }
 
         public void withAdditionalParams(Dictionary<string, string> additionalParameters)
         {
-            throw new NotImplementedException();
+            Debug.Log("Call to withAdditionalParams on not supported platform." + DummyMessage);
         }
 
         public IHSInAppPurchase build()
         {
-            throw new NotImplementedException();
+            Debug.Log("Call to withAdditionalParams on not supported platform." + DummyMessage);
+            return null;
         }
     }
 }
