5a331183fb2aaaf83cb9005cda278b977d64e55a
KulArtsiom <kulartsiom@gmail.com>
2021-07-19 11:11:51 +0300

    Update HolisticSolutionDemo.cs

--- Assets/StackHolisticSolution/Demo/HolisticSolutionDemo.cs
+++ Assets/StackHolisticSolution/Demo/HolisticSolutionDemo.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using AppodealAds.Unity.Api;
-using DefaultNamespace;
 using StackHolisticSolution;
 using UnityEngine;
 
@@ -12,39 +10,22 @@ using UnityEngine;
 public class HolisticSolutionDemo : MonoBehaviour, IHSAppInitializeListener, IHSInAppPurchaseValidateListener,
     IInAppPurchaseValidationiOSCallback
 {
-    
     #region Application keys
 
 #if UNITY_EDITOR && !UNITY_ANDROID && !UNITY_IPHONE
         public static string appKey = "";
 #elif UNITY_ANDROID
-       public static string appKey = "c05de97de46bf68a9ede523a580bef97e42692848736ecad";
+    public static string appKey = "c05de97de46bf68a9ede523a580bef97e42692848736ecad";
 #elif UNITY_IPHONE
     public static string appKey = "ae8558d35fbf2175d3e23ff61df138e27d3cd8efe1e789c4";
 #else
 	public static string appKey = "";
 #endif
-    
+
     #endregion
+
     void Start()
     {
-        StartCoroutine(waitInitializeFirebase());
-
-       
-
-
-// #if UNITY_ANDROID
-
-// #elif UNITY_IOS
-//         HSApp.validateInAppPurchaseiOS("productIdentifier", "price", "currency", "transactionId",
-//             "additionalParams", this);
-// #endif
-    }
-
-    private IEnumerator waitInitializeFirebase()
-    {
-        yield return new WaitUntil(() => FirebaseController.IsInitialized);
-        
         HolisticSolutionInitialize();
     }
 
@@ -61,41 +42,6 @@ public class HolisticSolutionDemo : MonoBehaviour, IHSAppInitializeListener, IHS
             .setAdType(Appodeal.INTERSTITIAL | Appodeal.REWARDED_VIDEO);
 
         HSApp.initialize(appConfig, this);
-        
-#if UNITY_ANDROID
-
-        var firstPurchase = new HSInAppPurchase.Builder(PurchaseType.Subscription)
-                .withPublicKey("YOUR_PUBLIC_KEY")
-                .withSignature("YOUR_SIGNATURE") 
-                .withPurchaseData("YOUR_PURCHASE_DATA")
-                .withPrice("0.01")
-                .withCurrency("0.02")
-                .withAdditionalParams(new Dictionary<string, string>
-                {
-                    {
-                        "key", "value"
-                    }
-                })
-                .withPurchaseTimestamp(213123)
-                .withPurchaseToken("token")
-                .withSku("sku")
-                .build();
-            
-        
-        HSApp.validateInAppPurchaseAndroid(firstPurchase, this);
-#endif
-        
-        // HSApp.validateInAppPurchaseiOS( 
-        //     "productIdentifier",  
-        //     "price",  
-        //     "currency",
-        //      "transactionId",
-        //      "additionalParams", 
-        //      iOSPurchaseType.consumable, 
-        //     this);
-        
-        
-        
     }
 
     #region HSAppInitializeListener
@@ -105,7 +51,7 @@ public class HolisticSolutionDemo : MonoBehaviour, IHSAppInitializeListener, IHS
         Debug.Log("onAppInitializeFailed");
 
         if (hsErrors == null) return;
-        
+
         foreach (var error in hsErrors)
         {
             Debug.Log($"HSApp: [Error]: " + error.toString());
@@ -128,7 +74,7 @@ public class HolisticSolutionDemo : MonoBehaviour, IHSAppInitializeListener, IHS
     }
 
     #endregion
-    
+
     #region HSInAppPurchaseValidateListener
 
     public void onInAppPurchaseValidateSuccess(HSInAppPurchase purchase, IEnumerable<HSError> errors)
