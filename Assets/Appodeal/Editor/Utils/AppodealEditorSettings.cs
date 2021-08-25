5f950f8214631570e197764a2895e06697da2720
KulArtsiom <kulartsiom@gmail.com>
2021-08-25 11:47:13 +0300

    Update AppodealEditorSettings.cs

--- Assets/Appodeal/Editor/Utils/AppodealEditorSettings.cs
+++ Assets/Appodeal/Editor/Utils/AppodealEditorSettings.cs
@@ -1,7 +1,4 @@
 #if UNITY_EDITOR
-#if UNITY_2018_1_OR_NEWER
-using Appodeal.Editor.AppodealManager.AppodealDependencies;
-#endif
 using UnityEditor;
 using UnityEngine;
 using AppodealAds.Unity.Editor.Checkers;
@@ -21,14 +18,6 @@ namespace AppodealAds.Unity.Editor.Utils
         {
             Application.OpenURL("http://www.appodeal.com");
         }
-
-// #if UNITY_2018_1_OR_NEWER        
-//         [MenuItem("Appodeal/Manage Appodeal SDK")]
-//         public static void AppodealSdkManager()
-//         {
-//             AppodealAdapterManager.ShowSdkManager();
-//         }
-// #endif
 
         [MenuItem("Appodeal/Appodeal Settings")]
         public static void SetAdMobAppId()
