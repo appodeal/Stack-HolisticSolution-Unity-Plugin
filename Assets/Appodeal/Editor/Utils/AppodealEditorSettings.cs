552238de76196a664f0dbe28a1db94cf26b03567
KulArtsiom <kulartsiom@gmail.com>
2021-08-25 17:08:44 +0300

    Update AppodealEditorSettings.cs

--- Assets/Appodeal/Editor/Utils/AppodealEditorSettings.cs
+++ Assets/Appodeal/Editor/Utils/AppodealEditorSettings.cs
@@ -7,25 +7,31 @@ namespace AppodealAds.Unity.Editor.Utils
 {
     public class AppodealEditorSettings : ScriptableObject
     {
-        [MenuItem("Appodeal/SDK Documentation")]
-        public static void OpenDocumentation()
+        [MenuItem("Holistic Solution/Appodeal SDK Documentation")]
+        public static void OpenAppodealDocumentation()
         {
             Application.OpenURL("http://www.appodeal.com/sdk/choose_framework?framework=2&full=1&platform=1");
         }
+        
+        [MenuItem("Holistic Solution/HS SDK Documentation")]
+        public static void OpenHsDocumentation()
+        {
+            Application.OpenURL("https://github.com/appodeal/Stack-HolisticSolution-Unity-Plugin#readme");
+        }
 
-        [MenuItem("Appodeal/Appodeal Homepage")]
+        [MenuItem("Holistic Solution/Appodeal Homepage")]
         public static void OpenAppodealHome()
         {
             Application.OpenURL("http://www.appodeal.com");
         }
 
-        [MenuItem("Appodeal/Appodeal Settings")]
+        [MenuItem("Holistic Solution/Appodeal Settings")]
         public static void SetAdMobAppId()
         {
             AppodealInternalSettings.ShowAppodealInternalSettings();
         }
 
-        [MenuItem("Appodeal/Remove plugin")]
+        [MenuItem("Holistic Solution/Remove plugin")]
         public static void RemoveAppodealPlugin()
         {
             RemoveHelper.RemovePlugin();
