8062dc2c01fe9d04c345b56ba7c1d7696a2b6e36
KulArtsiom <kulartsiom@gmail.com>
2021-08-25 11:03:04 +0300

    Update HolisticSolution.cs

--- Assets/StackHolisticSolution/Api/HolisticSolution.cs
+++ Assets/StackHolisticSolution/Api/HolisticSolution.cs
@@ -7,7 +7,7 @@ namespace StackHolisticSolution
 {
     public static class HolisticSolution
     {
-        public const string HolisticSolutionPluginVersion = "2.0.0";
+        public const string HolisticSolutionPluginVersion = "2.0.1";
     }
 
     [SuppressMessage("ReSharper", "InconsistentNaming")]
